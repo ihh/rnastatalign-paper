c634a1dbd9bbe9a34d5124e6d14c45795a681643
Ian Holmes <yam@static-169-229-195-136.qb3.berkeley.edu>
2009-06-03 13:15:00 -0700

    Updated plots.r so simulations.pdf now includes TKF accuracy

--- simulations/plots.r
+++ simulations/plots.r
@@ -6,9 +6,11 @@ require (stats)
 toPDF = TRUE
 
 alignColor = "blue"
+alignTKFColor = "cyan"
 structureColor = "red"
 
 alignShape = 16
+alignTKFShape = 16
 structureShape = 17
 
 ## default plot filename
@@ -33,12 +35,17 @@ data = read.table ("inferred/indiegram.analysis", header = TRUE, sep = "\t")
 
 ## average datapoints for each outgroup length
 align = array()
+alignTKF = array()
 structure = array()
 bins = unique (data$z)
 for (bin in bins) {
   align = rbind (
     align,
     c (bin, mean (subset (data$AlignAcc,
+                          data$z == bin))))
+  alignTKF = rbind (
+    alignTKF,
+    c (bin, mean (subset (data$AlignAccTKF,
                           data$z == bin))))
   structure = rbind (
     structure,
@@ -48,11 +55,12 @@ for (bin in bins) {
 
 ## remove NA rows
 align = align[-1,]
+alignTKF = alignTKF[-1,]
 structure = structure[-1,]
 
 plot.new()
 plot.window (xlim = c (0, 2.5),
-             ylim = c (0.4, 1))
+             ylim = c (0, 1))
 axis (1,
       c (0, 0.5, 1, 1.5, 2, 2.5))
 axis (2)
@@ -63,13 +71,18 @@ title (xlab = "Outgroup branch length",
 points (align,
         col = alignColor, pch = alignShape, cex = 1)
 lines (lowess (align), col = alignColor, lwd = 1.5)
+
+points (alignTKF,
+        col = alignTKFColor, pch = alignTKFShape, cex = 1)
+lines (lowess (alignTKF), col = alignTKFColor, lwd = 1.5)
+
 points (structure,
         col = structureColor, pch = structureShape, cex = 1)
 lines (lowess (structure), col = structureColor, lwd = 1.5)
 
-legend ("bottomleft", legend = c ("Structural reconstruction accuracy", "Alignment accuracy"),
-        col = c (structureColor, alignColor),
-        pch = c (structureShape, alignShape),
+legend ("bottomleft", legend = c ("Structural reconstruction accuracy (TKFST ML)", "Alignment accuracy (TKFST ML)", "Alignment accuracy (TKF91 ML)"),
+        col = c (structureColor, alignColor, alignTKFColor),
+        pch = c (structureShape, alignShape, alignTKFShape),
         xjust = 1,
         inset = 0.02)
 
